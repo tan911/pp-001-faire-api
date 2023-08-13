6a46d77bbe727c972be0e7923336956c1bd5699c
Tan <jolanutan@gmail.com>
2023-08-13 21:45:58 +0800

    chore: add user test model

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+import { query } from '../utils/query.util';
+import ErrorHandler from '../utils/error.util';
+
+interface UserInfo {
+  id: number;
+  user_id: string;
+  username: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+async function getUserInfo(): Promise<UserInfo> {
+  try {
+    const response = await query('SELECT * FROM account');
+    return response;
+  } catch (err) {
+    throw new ErrorHandler('An error occured', 400);
+  }
+}
+
+export { getUserInfo };
