c26020c7595e6b4b240273022fc802c0eaa1137a
Tan <jolanutan@gmail.com>
2023-08-13 21:36:12 +0800

    chore: use express error middelware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import 'dotenv/config';
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 import chalk from 'chalk';
 
-import { userRouter } from './routes/user.routes';
-import morganMiddleWare from './middlewares/morgan.middleware';
-import ErrorHandler from './utils/error';
+import userRouter from './routes/user.routes';
+import logger from './middlewares/logger.middleware';
+import ErrorHandler from './utils/error.util';
 
 const app: Express = express();
 
 const port = process.env.PORT ?? 3000;
-app.use(morganMiddleWare);
+
+app.use(logger);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
@@ -20,19 +24,16 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/faire/user', userRouter);
 
-app.all('*', (req, res, next) => {
-  next(new ErrorHandler(`Cant't ${req.originalUrl} on this server`, 404));
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  next(new ErrorHandler(`Cant't find ${req.originalUrl} on this server`, 404));
 });
 
-// app.use((err, req: Request, res: Response, next: NextFunction) => {
-//   err.statusCode = err.statusCode ?? 500;
-//   err.status = err.status ?? 'error';
-
-//   res.status(err.statusCode).json({
-//     status: err.status,
-//     message: err.message,
-//   });
-// });
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    stats: 'fail',
+    message: err.message,
+  });
+});
 
 app.listen(port, () => {
   console.log(
