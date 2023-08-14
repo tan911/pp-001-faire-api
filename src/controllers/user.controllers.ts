df5cd382d21d834996d89785c98311bda74de291
Tan <jolanutan@gmail.com>
2023-08-15 00:14:01 +0800

    refactor: use utility error to catch in async func and to minimize the use of IIEF with try/catch in controllers

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,22 +1,35 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { getUserInfo } from '../models/user.model';
+import asyncWrapper from '../utils/async-error.util';
 
-export function getUser(req: Request, res: Response): void {
-  void (async function (): Promise<void> {
-    try {
-      const data = await getUserInfo();
-      res.status(200).json({
-        status: 'success',
-        data: {
-          data,
-        },
-      });
-    } catch (err) {
-      res.status(400).json({
-        status: 'Fail',
-        message: err,
-      });
-    }
-  })();
-}
+export const getUser = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await getUserInfo();
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data,
+      },
+    });
+  },
+);
+
+// export function getUser(req: Request, res: Response): void {
+//   void (async function (): Promise<void> {
+//     try {
+//       const data = await getUserInfo();
+//       res.status(200).json({
+//         status: 'success',
+//         data: {
+//           data,
+//         },
+//       });
+//     } catch (err) {
+//       res.status(400).json({
+//         status: 'Fail',
+//         message: err,
+//       });
+//     }
+//   })();
+// }
